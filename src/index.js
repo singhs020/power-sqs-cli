e53a647381b004e0c3be70eef609a5b4b9234186
Simranjeet Singh <simranjeet2589@gmail.com>
2019-06-13 11:13:18 +0530

    [Adding a basic structure for the cli]

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const {Command} = require("commander");
+const chalk = require("chalk");
+const {initSinkToSQS} = require("power-sqs");
+const {version, description} = require("../package.json");
+
+const program = new Command();
+
+program
+  .version(version)
+  .description(description)
+  .command("sinktosqs")
+  .option("-s, --source <sourceSQS>", "The url for the source SQS")
+  .option("-d, --dest <dq>", "The url for the destination SQS")
+  .action(cmd => {
+    if(!cmd.source) {
+      return console.log(chalk.red("The source SQS is required."));
+    }
+
+    if(!cmd.dest) {
+      return console.log(chalk.red("The destination SQS is required."));
+    }
+
+    const config = {
+      "source": {"url": cmd.source},
+      "destination": {"url": cmd.dest}
+    };
+
+    initSinkToSQS(config);
+  });
+
+
+  program
+  .parse(process.argv);
